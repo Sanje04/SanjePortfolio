60fefe3c5896c086bddc7cdb6d36a78eac0e9d29
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-10-05 13:27:25 -0400

    React Nav Bar Menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,16 @@ import "./topbar.scss"
 export default function topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">genius.</a>
+                    <div className="itemContainer">
+                        
+                    </div>
+                </div>
+                <div className="right">
+                </div>
+            </div>
         </div>
     )
 }
