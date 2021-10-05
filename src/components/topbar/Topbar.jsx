a4c713bea9924ad9b9b0e8d489b3c6a826cf00e7
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-10-05 13:46:59 -0400

    add material-ui icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import "./topbar.scss"
+import {PersonSharp, MailSharp, ContactPhoneSharp} from "@material-ui/icons"
 
 export default function topbar() {
     return (
@@ -7,10 +8,16 @@ export default function topbar() {
                 <div className="left">
                     <a href="#intro" className="logo">genius.</a>
                     <div className="itemContainer">
-                        
+                        <ContactPhoneSharp className="icon"/>
+                        <span>289-923-0203</span>
+                    </div>
+                    <div className="itemContainer">
+                        <MailSharp className="icon"/>
+                        <span>sanje.divakaran@gmail.com</span>
                     </div>
                 </div>
                 <div className="right">
+                    
                 </div>
             </div>
         </div>
