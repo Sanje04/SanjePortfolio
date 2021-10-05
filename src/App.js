4399f7a1c24a750e1a7cab1f782aad7204ad3a43
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-10-05 10:44:46 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Topbar from "./components/topbar/Topbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar />
+      Sec -intro -portfolio -work ....
+    </div>
+  );
+}
+
+export default App;
