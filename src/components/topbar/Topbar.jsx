2cfdc05019f82342d59253895380ff58297f56bc
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-10-06 12:41:49 -0400

    React sidebar menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -17,7 +17,7 @@ export default function topbar({menuOpen, setMenuOpen}) {
                     </div>
                 </div>
                 <div className="right">
-                    <div className="hamburger">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
                         <span className="line1"></span>
                         <span className="line2"></span>
                         <span className="line3"></span>
