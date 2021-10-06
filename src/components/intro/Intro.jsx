ec5f8a97acf9247a16768497132163636a22f116
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-10-06 16:44:05 -0400

    Introduction Page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,22 @@
 import "./intro.scss"
+import { ExpandMoreSharp } from "@material-ui/icons"
 
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Greetings! I'm</h2>
+                    <h1>Sanje Divakaran</h1>
+                    <h3>I am a <span>Software Developer</span></h3>
+                </div>
+                <ExpandMoreSharp className="icon" id="ExpandMoreSharp"/>
+            </div>
         </div>
     )
 }
