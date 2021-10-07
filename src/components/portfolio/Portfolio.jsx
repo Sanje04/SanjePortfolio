1c285644d66a7c550512454bebe08b3029b5b8ec
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-10-07 14:15:23 -0400

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,63 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Portfolio</li>
+                <li>Featured</li>
+                <li>Web App</li>
+                <li>Featured</li>
+                <li>Software</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img 
+                        src="https://zellusmarketing.com/wp-content/uploads/2021/03/icon-1.8s-266px-2.gif" 
+                        alt="" 
+                    />
+                    <h3>Youtube App</h3>
+                </div>
+
+                <div className="item">
+                    <img 
+                        src="https://zellusmarketing.com/wp-content/uploads/2021/03/icon-1.8s-266px-2.gif" 
+                        alt="" 
+                    />
+                    <h3>Youtube App</h3>
+                </div>
+
+                <div className="item">
+                    <img 
+                        src="https://zellusmarketing.com/wp-content/uploads/2021/03/icon-1.8s-266px-2.gif" 
+                        alt="" 
+                    />
+                    <h3>Youtube App</h3>
+                </div>
+                
+                <div className="item">
+                    <img 
+                        src="https://zellusmarketing.com/wp-content/uploads/2021/03/icon-1.8s-266px-2.gif" 
+                        alt="" 
+                    />
+                    <h3>Youtube App</h3>
+                </div>
+
+                <div className="item">
+                    <img 
+                        src="https://zellusmarketing.com/wp-content/uploads/2021/03/icon-1.8s-266px-2.gif" 
+                        alt="" 
+                    />
+                    <h3>Youtube App</h3>
+                </div>
+
+                <div className="item">
+                    <img 
+                        src="https://zellusmarketing.com/wp-content/uploads/2021/03/icon-1.8s-266px-2.gif" 
+                        alt="" 
+                    />
+                    <h3>Youtube App</h3>
+                </div>
+            </div>
         </div>
     )
 }
