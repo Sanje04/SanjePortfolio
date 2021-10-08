088141abca3e1334021de539c0ba4788ae9e26ef
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-10-08 19:01:00 -0400

    final commits

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,29 @@
 import "./contact.scss"
+import {} from "@material-ui/icons";
+import { useState } from "react";
 
 export default function Contact() {
+
+    const[message,setMessage] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+    } 
     return (
         <div className="contact" id="contact">
-            
+            <div className="left">
+                <img src="https://www.marketernity.com/wp-content/uploads/2020/04/undraw_email_campaign_qa8y.png" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Email"/>
+                    <textarea placeholder="Message"></textarea>
+                    <button>Send</button>
+                    {message && <span>Thanks, I will reply soon! </span>}
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,19 +1,51 @@
-import "./testimonials.scss"
+import "./testimonials.scss";
+import { ArrowRightSharp, Person, LinkedIn} from "@material-ui/icons"
 
 export default function Testimonials() {
+    const data = [
+        {
+            id: 1,
+            name: 'Joe Known',
+            title: "Senior Software Developer",
+            desc: "Sanje is a great programmer! (Dummy Text)",
+            img: "https://thumbs.dreamstime.com/b/attractive-men-work-office-mans-calculates-calculator-using-laptop-accounting-profession-man-sitting-desk-128696722.jpg"
+        },
+        {
+            id: 2,
+            name: 'Steve Rogers',
+            title: "Senior Software Engineer",
+            desc: "Sanje is a great programmer! (Dummy Text 2)",
+            img: "https://i.pinimg.com/474x/31/5c/8b/315c8b0bcfc40900ae1462551f6d1ebd.jpg"
+        },
+        {
+            id: 3,
+            name: 'LeBron Games',
+            title: "Senior Software Developer",
+            desc: "Sanje is a goat programmer! (Dummy Text 3)",
+            img: "https://cdn.cnn.com/cnnnext/dam/assets/210928172406-lebron-james-media-day-0928-large-169.jpg"
+        }
+    ];
+
     return (
         <div className="testimonials" id="testimonial">
-            <h1>testimonials</h1>
+            <h1>Testimonials</h1>
             <div className="container">
-                <div className="card">
-                    <div className="top">
-                        <img src="assets/right-arrow.png" alt="" />
-                        <img src="" alt="" />
-                        <img src="" alt="" />
+                {data.map((d)=>(
+                    <div className="card">
+                        <div className="top">
+                            <ArrowRightSharp className="left"/>
+                            <img src={d.img} alt="" />
+                            <LinkedIn className="right"/>
+                        </div>
+                        <div className="center">
+                            {d.desc}
+                        </div>
+                        <div className="bottom">
+                            <h3>{d.name}</h3>
+                            <h4>{d.title}</h4>
+                        </div>
                     </div>
-                    <div className="center"></div>
-                    <div className="bottom"></div>
-                </div>
+                ))}
             </div>
         </div>
     )
